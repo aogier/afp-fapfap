814ea1896913196722148c8b32209715f2f20f4a
Alessandro -oggei- Ogier <alessandro.ogier@ieo.eu>
2016-01-29 18:15:57 +0100

    tests

--- tests/whitespaces.py
+++ tests/whitespaces.py
@@ -8,22 +8,44 @@ from pkg_resources import Requirement, resource_filename  # @UnresolvedImport
 from tempfile import mkdtemp
 import os
 import shutil
+from afpfapfap.main import scandirs
+from pathlib import Path
 
 
 class TestWhitespace(unittest.TestCase):
 
     def setUp(self):
-        super.setUp()
+        super().setUp()
         self.tempdir = mkdtemp()
 
     def tearDown(self):
-        super.tearDown()
+        super().tearDown()
         shutil.rmtree(self.tempdir)
 
     def testLeadingWhitespaceRemoval(self):
 
-        os.mkdir(os.path.join(self.tempdir, ' dir'))
+        path = os.path.join(self.tempdir, ' dir')
+        normalized_path = os.path.join(self.tempdir, 'dir')
+
+        os.mkdir(path)
+        for entry in scandirs(Path(self.tempdir)):
+            pass
 
         self.assertTrue(
-            os.path.exists(os.path.join(self.tempdir, 'dir')),
-            'leading space not removed')
+            os.path.exists(normalized_path),
+            'leading space not removed: %s' % [x.path for x
+                                               in os.scandir(self.tempdir)])
+
+    def testTrailingWhitespaceRemoval(self):
+
+        path = os.path.join(self.tempdir, 'dir ')
+        normalized_path = os.path.join(self.tempdir, 'dir')
+
+        os.mkdir(path)
+        for entry in scandirs(Path(self.tempdir)):
+            pass
+
+        self.assertTrue(
+            os.path.exists(normalized_path),
+            'trailing space not removed: %s' % [x.path for x
+                                                in os.scandir(self.tempdir)])
